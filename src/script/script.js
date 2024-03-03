6726e4debc80f36fe2ebf2507871f1020e1138c8
Melo <102263606+Melo1337@users.noreply.github.com>
2024-03-03 01:21:32 +0000

    .

--- src/script/script.js
+++ src/script/script.js
@@ -1,39 +1,56 @@
-let optionOneSelected = null
+let optionOneSelected = null;
 
 function selectProductOne(option) {
     if (optionOneSelected !== null) {
         optionOneSelected.classList.remove("select");
     }
 
-    optionOneSelected = option
-    optionOneSelected.classList.add("select")
+    optionOneSelected = option;
+    optionOneSelected.classList.add("select");
+
+    checkOptionsSelected();
 }
 
-let optionTwoSelected = null
-
+let optionTwoSelected = null;
 
 function selectProductTwo(option) {
     if (optionTwoSelected !== null) {
         optionTwoSelected.classList.remove("select");
     }
 
-    optionTwoSelected = option
-    optionTwoSelected.classList.add("select")
+    optionTwoSelected = option;
+    optionTwoSelected.classList.add("select");
+
+    checkOptionsSelected();
 }
 
-let optionThreeSelected = null
+let optionThreeSelected = null;
 
 function selectProductThree(option) {
     if (optionThreeSelected !== null) {
         optionThreeSelected.classList.remove("select");
     }
 
-    optionThreeSelected = option
-    optionThreeSelected.classList.add("select")
+    optionThreeSelected = option;
+    optionThreeSelected.classList.add("select");
+
+    checkOptionsSelected();
 }
 
-if (optionOneSelected.classList.contains("select") && optionTwoSelected.classList.contains("select") && optionThreeSelected.classList.contains("select")) {
-    let button = document.getElementsByTagName("button")
-    button[0].classList.add("select")
+function checkOptionsSelected() {
+    if (
+        optionOneSelected !== null &&
+        optionTwoSelected !== null &&
+        optionThreeSelected !== null &&
+        optionOneSelected.classList.contains("select") &&
+        optionTwoSelected.classList.contains("select") &&
+        optionThreeSelected.classList.contains("select")
+    ) {
+        let button = document.getElementsByTagName("button");
+        button[0].classList.add("buttonSelect");
+    }
 }
 
+function closeOrder(button) {
+    
+}
